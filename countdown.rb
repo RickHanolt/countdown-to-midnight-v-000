c82bb5c7ea4703073607e4be106d5716a22a449e
Richard Hanolt <rhanolt@gmail.com>
2020-05-01 04:10:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,6 +6,11 @@ def countdown(count)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-
+def countdown_with_sleep(count)
+  until count == 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+    sleep
+  end
+  "HAPPY NEW YEAR!"
 end
